f09aedd1f845a86f8cdbbe1291dc2bb3f95ed04e
Snail <soykim76@gmail.com>
2020-06-09 13:27:43 +0900

    기초 문법

--- 03_slicing.py
+++ 03_slicing.py
@@ -3,6 +3,7 @@ print(sample_list)
 print(sample_list[1:3])
 print(sample_list[10:-1]) 
 print(sample_list[0:len(sample_list) :3])
+# 슬라이싱에서 생략
 print(sample_list[0::3]) # 처음부터 끝까지, 3 스탭
 print(sample_list[::3]) # 처음부터 끝까지, 3 스텝
 print(sample_list[::-1]) # 처음부터 끝까지, 역순으로
---
+++ 05_if_statements.py
@@ -0,0 +1,15 @@
+x = 100
+
+# python style guide 지키자
+# 4 spaces --- 1 indentation
+if x < 0:
+    print('Negative')
+elif x == 0:
+    print('Zero')
+elif x == 0:
+    print('Single')
+else:
+    print('More')
+
+
+
